2ab720c7be1a024dee19b37973a65f4e6d734532
dovydasdzezulskis <dovydas.dzezulskis@unity3d.com>
2019-02-19 14:08:26 +0200

    zinute

---
+++ Assets/Dovydas/FreeMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    float lookSensitivity = 50;
+    public float speed = 100f;
+
+    float rotationX = 0.0f;
+    float rotationY = 0.0f;
+
+    public float minX, maxX, minZ, maxZ, minY, maxY;
+    public float mult;
+    public Vector3 lastPosition;
+
+    public bool needsLock;
+    public float zoomspeed;
+
+    void Start()
+    {
+        
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * zoomspeed * Time.deltaTime * Input.GetAxis("Mouse ScrollWheel"));
+
+        needsLock = false;
+        if (Input.GetMouseButton(2))
+        {
+            //needsLock = true;
+            transform.Translate(-(Input.mousePosition - lastPosition) * mult);
+        }
+        lastPosition = Input.mousePosition;
+
+        if (Input.GetMouseButton(1))
+        {
+            needsLock = true;
+            //Cursor.lockState = CursorLockMode.Locked;
+            rotationX += Input.GetAxis("Mouse X") * lookSensitivity * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * lookSensitivity * Time.deltaTime;
+            rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+            transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+            rb.velocity = Vector3.zero;
+            transform.Translate(new Vector3(Vector3.forward.x, 0f, Vector3.forward.z) * Input.GetAxis("Vertical") * Time.deltaTime * speed);
+            transform.Translate(new Vector3(Vector3.right.x, 0f, Vector3.right.z) * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+
+           
+
+            
+        }
+
+        if(needsLock)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
+
+
+
+        if (transform.position.x >= maxX)
+        {
+            transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x <= -minX)
+        {
+            transform.position = new Vector3(-minX, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y >= maxY)
+        {
+            transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+        }
+        if (transform.position.y <= -minY)
+        {
+            transform.position = new Vector3(transform.position.x, -minY, transform.position.z);
+        }
+
+        if (transform.position.z >= maxZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, maxZ);
+        }
+        if (transform.position.z <= -minZ)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -minZ);
+        }
+
+    }
+
+
+
+}
---
+++ Assets/Dovydas/UIController.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+enum Action { Passive = 0, Attack = 1, SpecialMove = 2, Move = 3 };
+
+public class UIController : MonoBehaviour
+{
+    #region Variables
+    public int manaLeft, maxMana, manaSavingMax, manaPerTurn, startingMana;
+
+    public bool unitIsSelected;
+    public bool needActionAndInfoZone;
+
+    public float[] heights;
+
+    [Range(1,15)]
+    public int manaCost;
+
+    public Text[] infoText_Text;
+    public string[] infoText_string;
+
+    public GameObject[] ActivateButton;
+    public GameObject[] BackgroundButton;
+    public GameObject[] ExitInfoButton;
+    public GameObject[] EnterInfoButton;
+    public GameObject[] infoBackground;
+
+    public GameObject infoAndActionsZone;
+
+    public RectTransform manaBar;
+    public float what = 100f;
+
+    public GameObject screen_JoinToServer,
+                      screen_MainMenu,
+                      screen_setup,
+                      screen_inGame,
+                      screen_localMultiplayer;
+
+    public Image[] mana;
+    public Sprite mana_empty, mana_full;
+    public int unitSelected;
+    #endregion
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Clicked_ToMainMenu();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            unitIsSelected = true;
+        }
+
+        if (needActionAndInfoZone && unitIsSelected)
+            infoAndActionsZone.SetActive(true);
+        else
+            infoAndActionsZone.SetActive(false);
+
+        ManaCostBarUpdate();
+    }
+
+
+    public void Clicked_Activate(int id)
+    {
+        Debug.Log("Selected action: " + id);
+    }
+
+    public void Clicked_UnitSelected(int id)
+    {
+        unitIsSelected = true;
+        unitSelected = id;
+        // use infoText_Text[id].text = "your text";
+    }
+
+    public void InitializeGame()
+    {
+        manaLeft = startingMana;
+        for (int i = 0; i < maxMana; i++)
+        {
+            if (i < manaLeft)
+                mana[i].sprite = mana_full;
+            else
+                mana[i].sprite = mana_empty;
+        }
+    }
+
+    public void Clicked_NextTurn()
+    {
+        #region Mana
+        int manaLeftover = manaLeft;
+        if(manaLeft > manaSavingMax)
+        {
+            manaLeft = manaLeft + manaSavingMax;
+        }
+        else
+        {
+            manaLeft += manaPerTurn;
+        }
+        
+        if(manaLeft > maxMana)
+        {
+            manaLeft = maxMana;
+        }
+
+        for (int i = 0; i < maxMana; i++)
+        {
+            if (i < manaLeft)
+                mana[i].sprite = mana_full;
+            else
+                mana[i].sprite = mana_empty;
+        }
+        #endregion
+
+    }
+
+    public void ManaCostBarUpdate()
+    {
+        if (manaCost > 0 && manaCost < 16)
+        {
+            manaBar.gameObject.SetActive(true);
+            manaBar.sizeDelta = new Vector2(manaBar.sizeDelta.x, heights[manaCost - 1]);
+        }
+        else
+            manaBar.gameObject.SetActive(false);
+    }
+
+    #region Main UI functionality
+    public void Clicked_Info(int id)
+    {
+        infoText_Text[id].text = infoText_string[id];
+        infoBackground[id].SetActive(true);
+    }
+
+    public void Clicked_ExitInfo(int id)
+    {
+        infoBackground[id].SetActive(false);
+    }
+
+    public void Clicked_ToLocalMultiplayed()
+    {
+        screen_localMultiplayer.SetActive(true);
+        screen_JoinToServer.SetActive(false);
+        screen_MainMenu.SetActive(false);
+        screen_setup.SetActive(false);
+        screen_inGame.SetActive(false);
+        needActionAndInfoZone = false;
+    }
+
+    public void Clicked_ToJoinServer()
+    {
+        screen_localMultiplayer.SetActive(false);
+        screen_JoinToServer.SetActive(true);
+        screen_MainMenu.SetActive(false);
+        screen_setup.SetActive(false);
+        screen_inGame.SetActive(false);
+        needActionAndInfoZone = false;
+    }
+
+    public void Clicked_ToMainMenu()
+    {
+        screen_localMultiplayer.SetActive(false);
+        screen_JoinToServer.SetActive(false);
+        screen_MainMenu.SetActive(true);
+        screen_setup.SetActive(false);
+        screen_inGame.SetActive(false);
+        infoAndActionsZone.SetActive(false);
+        needActionAndInfoZone = false;
+    }
+
+    public void Clicked_ToInGame()
+    {
+        unitIsSelected = false;
+        screen_localMultiplayer.SetActive(false);
+        screen_JoinToServer.SetActive(false);
+        screen_MainMenu.SetActive(false);
+        screen_setup.SetActive(false);
+        screen_inGame.SetActive(true);
+        needActionAndInfoZone = true;
+        InitializeGame();
+    }
+    public void ToSetup()
+    {
+        unitIsSelected = false;
+        screen_localMultiplayer.SetActive(false);
+        screen_JoinToServer.SetActive(false);
+        screen_MainMenu.SetActive(false);
+        screen_setup.SetActive(true);
+        screen_inGame.SetActive(false);
+        needActionAndInfoZone = true;
+    }
+
+    public void Clicked_Setup_createdServer()
+    {
+        ToSetup();
+
+    }
+
+    public void Clicked_Setup_joinedServer()
+    {
+        ToSetup();
+
+    }
+
+    public void Clicked_Setup_localMultiplayer()
+    {
+        ToSetup();
+
+    }
+    #endregion
+}
