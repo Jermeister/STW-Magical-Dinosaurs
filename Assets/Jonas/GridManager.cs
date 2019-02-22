4dd9776ac71a4531613d25012f826d64185bba5d
Liudas Spanguole <liudas@unity3d.com>
2019-02-22 16:59:39 +0200

    Lerping movement and rotation

--- Assets/Jonas/GridManager.cs
+++ Assets/Jonas/GridManager.cs
@@ -315,8 +315,8 @@ public class GridManager : MonoBehaviour
                 index = i;
             }
         }
-        SpawnedObjects[index].transform.position = canMoveObjects[targetPos.x].column[targetPos.y].transform.position + new Vector3(0f, 0.17f, 0f);
-        SpawnedObjects[index].GetComponent<Dinosaur>().tileX = targetPos.x;
+	    SpawnedObjects[index].transform.position = canMoveObjects[targetPos.x].column[targetPos.y].transform.position + new Vector3(0f, 0.17f, 0f);
+		SpawnedObjects[index].GetComponent<Dinosaur>().tileX = targetPos.x;
         SpawnedObjects[index].GetComponent<Dinosaur>().tileZ = targetPos.y;
 
         TileTypeMap[tileX, tileZ] = 0;
